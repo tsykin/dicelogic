306a81d0470ef5cbe2c798f8897e36e2e2faa9a3
tsykin <tsikinsasha@gmail.com>
2024-03-23 14:01:56 +0100

    init commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,112 +1,127 @@
-import Image from "next/image";
+"use client";
+import React, { useEffect, useState } from "react";
 
 export default function Home() {
+  const [numbers, setNumbers] = useState([]);
+  const [numberCounts, setNumberCounts] = useState(new Array(11).fill(0));
+  const [lastRolledNumber, setLastRolledNumber] = useState(null);
+
+  function weightedRandom() {
+    // Define the probabilities for each number (2-12)
+    const probabilities = [
+      2.78, // 2
+      5.56, // 3
+      8.33, // 4
+      11.11, // 5
+      13.89, // 6
+      13.89, // 7
+      13.89, // 8
+      11.11, // 9
+      8.33, // 10
+      5.56, // 11
+      2.78, // 12
+    ];
+
+    // Calculate the total weight
+    const totalWeight = probabilities.reduce((acc, val) => acc + val, 0);
+
+    // Generate a random number between 0 and the total weight
+    const random = Math.random() * totalWeight;
+
+    // Loop through the probabilities and find which one matches the random number
+    let cumulativeProbability = 0;
+    for (let i = 0; i < probabilities.length; i++) {
+      cumulativeProbability += probabilities[i];
+      if (random < cumulativeProbability) {
+        return i + 2; // Adding 1 to make it 1-based index
+      }
+    }
+  }
+
+  useEffect(() => {
+    updateNumberCounts();
+  }, [numbers]); // Run updateNumberCounts whenever numbers state changes
+
+  function updateNumberCounts() {
+    const counts = new Array(11).fill(0);
+    numbers.forEach((number) => {
+      counts[number - 2]++;
+    });
+    setNumberCounts(counts);
+  }
+
+  function generate1Number() {
+    const nextNumber = weightedRandom();
+    setNumbers((prevNumbers) => [...prevNumbers, nextNumber]);
+    setLastRolledNumber(nextNumber);
+    updateNumberCounts();
+  }
+
+  function generate50Numbers() {
+    for (let i = 0; i < 50; i++) {
+      const nextNumber = weightedRandom();
+      setNumbers((prevNumbers) => [...prevNumbers, nextNumber]);
+      setLastRolledNumber(nextNumber);
+    }
+    updateNumberCounts();
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="flex flex-col items-center justify-between py-4">
+      <div className="flex flex-col bg-blue-100 gap-1 py-4 pr-4">
+        {/* Bar chart */}
+        {numberCounts.map((count, index) => (
+          <div key={index} className="flex flex-row gap-1 items-center">
+            <div
+              style={{
+                paddingRight: `${count * 0.75}rem`,
+                paddingLeft: "4px",
+              }}
+              className="bg-blue-800 flex flex-row items-center justify-start rounded-r-lg h-6"
+            >
+              <span className="text-white text-xs">
+                {count > 0 && index + 2}
+              </span>
+            </div>
+            {count}
+          </div>
+        ))}
+      </div>
+      <div className="flex flex-col mt-2">
+        {/* <button
+          onClick={generate50Numbers}
+          className="bg-blue-300 px-4 py-2 rounded m-1"
+        >
+          Get next 50 numbers
+        </button> */}
+        <button
+          onClick={generate1Number}
+          className="bg-blue-300 px-4 py-2 rounded m-1"
+        >
+          Get next number
+        </button>
+      </div>
+      <div className="flex flex-col my-4">
+        {/* Show total number of number in array */}
+        <span className="text-center text-xl">
+          Total amount of rolls: {numbers.length}
+        </span>
+        {/* Show last rolled number */}
+        {lastRolledNumber && (
+          <span className="text-center text-xl">
+            Last rolled number: {lastRolledNumber}
+          </span>
+        )}
+      </div>
+      <div className="grid grid-cols-6 gap-4">
+        {numbers.map((number) => (
+          <p
+            key={number}
+            className="text-center bg-zinc-200 rounded-lg p-1 border border-zinc-300"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+            {number}
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        ))}
       </div>
     </main>
   );
